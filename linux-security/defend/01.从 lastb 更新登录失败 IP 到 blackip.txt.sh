5f2374dc664ab7579b5a77f5db6050bdd24c5382
pysense <pysense@gmail.com>
2021-09-15 15:10:32 +0800

    2021/09/15 Update

--- linux-security/defend/01.从 lastb 更新登录失败 IP 到 blackip.txt.sh
+++ linux-security/defend/01.从 lastb 更新登录失败 IP 到 blackip.txt.sh
@@ -2,7 +2,8 @@
 for a in $(
     for i in $(find /var/log -type f -name btmp*); do
         lastb -f $i
-    done | awk '/ssh:/{if(/^[ ]/){print$2}else{print$3}}' | sort | uniq -c | sort -rn | awk '$1>=5{print$2}'
+    done | awk '/ssh:/{if(/^[ ]/){print$2}else{print$3}}' | sort | uniq -c | sort -rn |
+           awk '$1>=5{print$2}' # 打印失败次数大等于 5 的 IP
 ); do
     if ! grep -q "^$a$" blackip.txt; then
         echo "Add IP to blackip.txt: $a"
