53dc6e6abfe69511d14c247a6f902498a80fead5
pysense <pysense@gmail.com>
2021-09-15 23:49:28 +0800

    2021/09/15 Update

--- linux-security/defend/02.从 aureport 更新登录失败 IP 到 blackip.txt.sh
+++ linux-security/defend/02.从 aureport 更新登录失败 IP 到 blackip.txt.sh
@@ -1,8 +1,6 @@
 #/bin/bash
 for a in $(
-    for i in $(find /var/log -type f -name btmp*); do
-        lastb -f $i
-    done | awk '/ssh:/{if(/^[ ]/){print$2}else{print$3}}' | sort | uniq -c | sort -rn |
+    aureport -l --failed | awk '{print$5}' | sort | uniq -c | sort -rn |
         awk '$1>=5{print$2}' # 打印失败次数大等于 5 的 IP
 ); do
     if ! grep -q "^$a$" blackip.txt; then
